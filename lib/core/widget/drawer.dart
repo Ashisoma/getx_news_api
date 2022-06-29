e7f05a75b1e3574310feff66a75342f453e201b8
Ashisoma <okalionesmus@gmail.com>
2022-06-29 19:14:38 +0300

    drawer

---
+++ lib/core/widget/drawer.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+getAppDrawer() {
+  return Drawer(
+    child: ListView(
+      children: [
+        ListTile(
+          title: Text("News Headline"),
+          onTap: () {},
+          leading: Icon(Icons.contact_page_outlined),
+        ),
+        ListTile(
+          title: Text("Search"),
+          onTap: () {},
+          leading: Icon(Icons.search),
+        )
+      ],
+    ),
+  );
+}
---
+++ lib/features/news_headlines/view/news_headline.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:getx_news_api/core/widget/drawer.dart';
+
+class NewsHeadline extends StatelessWidget {
+  const NewsHeadline({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("News Headline"),
+      ),
+      drawer: getAppDrawer(),
+      body: Container(),
+    );
+  }
+}
